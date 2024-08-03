497eadd1f340799084119ebda5eb3df385f6b3eb
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-08-03 17:45:08 +0700

    update

--- src/middleware/removeImage.mjs
+++ src/middleware/removeImage.mjs
@@ -1,70 +1,71 @@
-const fs = require("fs");
+import { promises as fs } from "fs";
 
-export const handleRemoveLogo = (path) => {
+export const handleRemoveLogo = async (path) => {
     if (path) {
-        fs.unlink(`./src/public/logoTeams/${path}`, (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-        });
-        return true;
+        try {
+            await fs.unlink(`./src/public/logoTeams/${path}`);
+            return true;
+        } catch (err) {
+            console.error(err);
+            return false;
+        }
     } else {
         return false;
     }
 };
 
-export const handleRemoveAvatar = (path) => {
+export const handleRemoveAvatar = async (path) => {
     if (path) {
-        fs.unlink(`./src/public/avatarPlayers/${path}`, (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-        });
-        return true;
+        try {
+            await fs.unlink(`./src/public/avatarPlayers/${path}`);
+
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
     } else {
         return false;
     }
 };
 
-export const handleRemoveMatch = (path) => {
+export const handleRemoveMatch = async (path) => {
     if (path) {
-        fs.unlink(`./src/public/matchs/${path}`, (err) => {
-            if (err) {
-                console.error(err);
-                return false;
-            }
-        });
-        return true;
+        try {
+            await fs.unlink(`./src/public/matchs/${path}`);
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
     } else {
         return false;
     }
 };
 
-export const handleRemoveStadiumImage = (path) => {
+export const handleRemoveStadiumImage = async (path) => {
     if (path) {
-        fs.unlink(`./src/public/stadiums/${path}`, (err) => {
-            if (err) {
-                console.error(err);
-                return false;
-            }
-        });
-        return true;
+        try {
+            await fs.unlink(`./src/public/stadiums/${path}`);
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
     } else {
         return false;
     }
 };
 
-export const handleRemoveUserAvatar = (path) => {
+export const handleRemoveUserAvatar = async (path) => {
     if (path) {
-        fs.unlink(`./src/public/avatarUsers/${path}`, (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-        });
-        return true;
+        try {
+            await fs.unlink(`./src/public/avatarUsers/${path}`);
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
     } else {
         return false;
     }
