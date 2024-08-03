641be1a835c77bfb66dac78f7331653679cfbfb2
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-08-03 22:49:42 +0700

    update

--- src/config/connectDb.mjs
+++ src/config/connectDb.mjs
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE, "root", null, {
+const sequelize = new Sequelize(process.env.DATABASE, "root", "password", {
     host: "localhost",
     dialect: "mysql",
 });
