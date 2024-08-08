3d08af949e886accff392f8c64a57a55af2f20dc
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-08-08 17:12:18 +0700

    up

--- src/config/configCorsNew.mjs
+++ src/config/configCorsNew.mjs
@@ -3,33 +3,4 @@ import cors from "cors";
 
 dotenv.config();
 
-export default function configCorsNew(app) {
-    const corsOptions = {
-        origin: [
-            "https://fe-nha-admin.vercel.app",
-            "http://localhost:3000",
-            "http://localhost:5173",
-        ],
-        credentials: true,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-        allowedHeaders: "Content-Type,Authorization",
-    };
-
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-        res.header(
-            "Access-Control-Allow-Methods",
-            "GET, POST, PUT, DELETE, OPTIONS"
-        );
-        if (req.method === "OPTIONS") {
-            return res.sendStatus(200);
-        }
-        next();
-    });
-
-    app.use(cors(corsOptions));
-}
+export default function configCorsNew(app) {}
