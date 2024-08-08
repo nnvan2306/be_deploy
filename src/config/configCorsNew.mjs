00ffba7c15f59094f0812db0835208abaf3a4fb4
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-08-08 17:12:44 +0700

    up

--- src/config/configCorsNew.mjs
+++ src/config/configCorsNew.mjs
@@ -3,4 +3,15 @@ import cors from "cors";
 
 dotenv.config();
 
-export default function configCorsNew(app) {}
+export default function configCorsNew(app) {
+    const corsOption = {
+        origin: [
+            "https://fe-nha-admin.vercel.app",
+            "http://localhost:3000",
+            "http://localhost:5173",
+        ],
+        credentials: true,
+    };
+
+    app.use(cors(corsOption));
+}
