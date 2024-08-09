6ef35bf4250b5aff41b9d002da425e6800253f86
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-08-09 10:16:01 +0700

    up

--- src/service/billService.mjs
+++ src/service/billService.mjs
@@ -76,6 +76,7 @@ const getLimitBillService = async (page, pageSize) => {
             include: [
                 {
                     model: db.Ticket,
+                    as: "TicketAlias",
                     include: [
                         {
                             model: db.Calendar,
