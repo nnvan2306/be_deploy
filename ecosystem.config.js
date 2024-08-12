3be2d5286e5ccfebbe5d320e98667ad44e5e80c9
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-08-12 21:24:00 +0700

    up

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: "be_deploy",
+            script: "./src/server.js",
+            env: {
+                PORT: 8080,
+                NEXT_URL: "https://fe-nha-production.vercel.app",
+                JWT_SECRET: "ngongocvan",
+                JWT_SERECT_REFRESH: "vanngo",
+                JWT_EXPIRES_ACCESS: "3s",
+                JWT_EXPIRES_REFRESH: "365d",
+                MAX_SIZE: 1000 * 1000 * 1,
+                MAX_SIZE_MATCH: 1000 * 1000 * 1,
+                EMAIL_USENAME: "vanvanvan23062003@gmail.com",
+                EMAIL_PASSWORD: "lcpw wjba lggo ybyq",
+                REACT_URL: "https://fe-nha-admin.vercel.app",
+            },
+        },
+    ],
+};
