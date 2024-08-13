ac49356a7583cd05c2eb1d76f1037d753e5dae0e
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-08-13 15:40:08 +0700

    up

--- src/middleware/jwtAction.mjs
+++ src/middleware/jwtAction.mjs
@@ -77,7 +77,6 @@ export const handleCheckRoleAdmin = (req, res) => {
     try {
         const path = req.headers.cookie;
 
-        console.log("token >>>", req.headers);
         if (!path) {
             return res.status(400).json(funcReturn("token empty ", 1, []));
         }
